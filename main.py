b161cf586b0cac199b5d641a3a87b00469fa4faf
ShinHyeoncheol <guscjf5806@icloud.com>
2024-04-29 15:31:15 +0900

    첫번째 커밋 파일 생성 및 계산기 코딩

---
+++ main.py
@@ -0,0 +1,13 @@
+##매서드 (=함수) 부분
+
+def add_func(n1,n2):
+    retVal = n1 + n2
+    return retVal
+
+## 전역 변수 (=클래스 부분)
+num1,num2,res = 100,200,0
+
+
+## 메인 함수 부분
+res = add_func(num1,num2)
+print(num1,'+',num2,'=',res)
