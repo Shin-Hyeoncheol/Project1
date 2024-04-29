e7ed2937a4b1971e77662009d5be928018d1e6be
ShinHyeoncheol <guscjf5806@icloud.com>
2024-04-29 15:39:20 +0900

    세번째 커밋 main.py에 mul 추가

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def sub_func(n1,n2):
     retVal = n1 - n2
     return retVal
 
+def mul_func(n1,n2):
+    retVal = n1 * n2
+    return retVal
+
+
 ## 전역 변수 (=클래스 부분)
 num1,num2,res = 100,200,0
 
@@ -18,3 +23,6 @@ print(num1,'+',num2,'=',res)
 
 res = sub_func(num1,num2)
 print(num1,'-',num2,'=',res)
+
+res = mul_func(num1,num2)
+print(num1,'*',num2,'=',res)
